f1ec619c1c06ed6882bdcd380ed1f7129a755faa
NOUIRA <ilhem.nouira@lepoles.org>
2024-05-29 16:37:29 +0200

    js

---
+++ Initiation_js/Cour/js/base.js
@@ -0,0 +1,59 @@
+// syntaxe de base
+
+/*
+commentaires sur plusieurs lignes.
+*/
+
+// // Déclarer une variable:
+// var prenom;
+// var prénomPourMonProjet;
+
+// // -- 2 : Affecter une valeurà une variable
+// prenom = "Ilhem";
+
+// var prenom2 = "Sirine";
+// alert(prenom);
+
+// // -- 3 : une instruction se termine TOUJOURS par un point-virgule, aussi il est possible d'ecrire plusieurs instructions sur une seule ligne
+// // inst_1;
+// // inst_2; inst_3;
+
+// // -- 5 : Afficher dans la console (ici, la valeur de ma variable prenom)
+// console.log(prenom);
+
+// // document.write(monNom + <br></br>);
+// // document.write(prenom + <br></br>);
+// // document.write("A la pause vous aurez des haribos PIK !" + <br></br>);
+
+// var jourSemaine = window.prompt("question: On est quel jour ?", "jour de la semaine");
+
+// document.write(jourSemaine);
+// console.log("jour de la semaine");
+// console.log("mercredi");
+// console.log(jourSemaine);
+
+
+// -- 9 : Une variablene peut pas commencerpar un chiffre, et ne doit contenir
+var chaineDecaractere = "ma variable";
+var unChiffre = 2;
+var cetteAnnee = 2024;
+var strinAddition = "2+5";
+var addition2Number = 2 + 6;
+
+alert(chaineDecaractere);
+document.write(unChiffre);
+console.log(cetteAnnee);
+console.log(strinAddition);
+console.log(addition2Number);
+
+
+
+
+
+
+
+
+
+
+
+
