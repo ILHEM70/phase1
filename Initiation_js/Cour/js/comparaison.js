0727b3233b8bc4112b307234e8c3ce076af8a9e4
NOUIRA <ilhem.nouira@lepoles.org>
2024-05-31 16:57:13 +0200

    Cour js

---
+++ Initiation_js/Cour/js/comparaison.js
@@ -0,0 +1,25 @@
+
+ 
+var prenom = "Ilhem"
+alert("Ilhem");
+var age = 54
+
+var texte = window.prompt("Quel est votre prénom ?");
+console.log(texte);
+console.log(typeof texte);
+
+if (prenomLogin = prenom) {
+    document.write("Bienvenue");
+    var saisiAge = window.prompt("Quel est votre âge ?");
+    if (saisiAge = age) {
+        document.write("Bienvenue");
+    }
+
+    
+} else {
+    alert()
+}
+
+
+
+
--- Initiation_js/Cour/js/concatenation.js
+++ Initiation_js/Cour/js/concatenation.js
@@ -4,11 +4,68 @@
 // var Concactenation = chaineDeCaractere + stringCour;
 // document.write("<h2>" + Concactenation + "</h2>");
 
-var annee = 2000;
-var futur = 24;
-var addition2Number = 2000 + 24;
-var stringAddition = "2000+24";
-document.write(stringAddition + "<br>");
-document.write(addition2Number);
-console.log(stringAddition);
-console.log(addition2Number);
+// var annee = 2000;
+// var futur = 24;
+// var addition2Number = 2000 + 24;
+// var stringAddition = "2000+24";
+// document.write(stringAddition + "<br>");
+// document.write(addition2Number);
+// console.log(stringAddition);
+// console.log(addition2Number);
+
+
+// var prenom = "Ilhem";
+// alert(prenom);
+
+// var texte = window.prompt("question: Qui suis-je ? ", "Ilhem");
+
+// document.write("<h1>" + voici mon text + "</h1>");
+
+//type variable boolean : true ou false
+
+// var unBooleanTrue = true;
+// var unBooleanFalse = false;
+
+// if (unBooleanTrue) {
+//     document.write(prenom + "<br>")
+// }
+// if (4 === "4"){
+// document.write("Ceuci est faux!");
+// }
+
+// var num1 = 50;
+// var num2 = 5;
+// var num3 = 10;
+// var num4 = 12.5;
+// var num5 = num1 / num3;
+
+// if (num1 / num3 != num4) {
+//     document.write(
+//         "le resultat de la division" + num1 + "par" + num3 + "est" + num5);
+// } else {
+//     document.write("la valeur de la varible num4 est egale à" + num4);
+// }
+
+
+
+var internaute = 18;
+alert(internaute);
+var texte = window.prompt("Inserez votre age");
+console.log(texte);
+console.log(typeof texte);
+
+texte = parseInt(texte);
+console.log(texte);
+console.log(typeof texte);
+
+if (texte >= internaute) {
+    document.write("Bienvenue");
+} else {
+    alert(" Vous êtes redirigés ver un site marchand");
+    document.location.href = "http://www.darty.com"
+}
+
+
+
+
+
