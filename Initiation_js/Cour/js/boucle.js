cdb69b2e40bb1e9387c9ff05bd030097249285fd
NOUIRA <ilhem.nouira@lepoles.org>
2024-06-06 16:53:21 +0200

    Continuation d'apprentissage

---
+++ Initiation_js/Cour/js/boucle.js
@@ -0,0 +1,31 @@
+// for (var i = 1; i <= 10; i += 2){
+//     document.write("<p>Instruction exécuter:" + i + "</P>");  //declarer une variable, mettre une condition , faire une incémentation( ou décrémenter ou multuplier...), puis executer la boucle
+// }
+// for (var j = 1; j <= 10; j++){
+//     document.write("<p>Inscription exécutée : " + j + "</p>");
+
+// }
+// for (var j; j <= 20; j += 2){
+//     document.write("<p> Instruction exécutée" + j + "</p>");
+// }
+// for (var mesSous = 1000; mesSous == 2000; mesSous += 50) {}
+
+// var tps = 0;
+// for (var mesSous = 1000; mesSous <= 2000; mesSous += 50) {
+//   document.write("Le mois suivant" + mesSous + "<br>");
+// }
+
+var majoritéFr = 18;
+var age = window.prompt("Inserrez votre âge : ");
+age = parseInt("message");
+//var age = parseInt(window.prompt("message"))
+while (age >= 3 || age <= 120);
+    
+
+
+
+// version 2 :
+// var age = prompt("message")
+// age = parseInt(age)
+
+
